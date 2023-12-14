75cb9bfcb4df600bf61705780523935f36cc52f5
Zhi Wang <oukore@gmail.com>
2023-12-13 17:12:15 -0800

    index0

--- trajtool/tfile.py
+++ trajtool/tfile.py
@@ -22,3 +22,26 @@ class TFile:
         if args.option == "superpose":
             self.refu = mda.Universe(args.input[0])
             self.refu.transfer_to_memory()
+
+    def index0_molecule_atoms(self) -> tuple[list[list[int]], list[int]]:
+        lst = []
+        start = 0
+        for na, nmol in zip(self.msizes, self.gsizes):
+            for _imol in range(nmol):
+                lst.append(list(range(start, start + na)))
+                start += na
+        pos = np.zeros(self.natoms)
+        for i, m in enumerate(lst):
+            pos[m] = i
+        return lst, pos.tolist()
+
+    def index0_residule_atoms(self) -> tuple[list[list[int], list[int]]]:
+        lst = []
+        for res in self.universe.residues:
+            l2 = [a.index for a in res.atoms]
+            l2.sort()
+            lst.append(l2)
+        pos = np.zeros(self.natoms)
+        for i, m in enumerate(lst):
+            pos[m] = i
+        return lst, pos.tolist()
